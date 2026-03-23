ce862b12613e4e40ce9768bac4797d19d924382f
Dmitry Kurochkin <kudima03@gmail.com>
2026-03-23 12:16:38 +0300

    AxisRelationalModelHash implemented

---
+++ src/Pure.Chart.RelationalModel.HashCodes/AxisRelationalModelHash.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using Pure.Chart.RelationalModel.Abstractions;
+using Pure.HashCodes;
+using Pure.HashCodes.Abstractions;
+using Pure.Primitives.Abstractions.Guid;
+using Pure.Primitives.Abstractions.String;
+
+namespace Pure.Chart.RelationalModel.HashCodes;
+
+public sealed record AxisRelationalModelHash : IDeterminedHash
+{
+    private static readonly byte[] TypePrefix =
+    [
+        247,
+        25,
+        157,
+        1,
+        50,
+        254,
+        158,
+        125,
+        188,
+        66,
+        0,
+        101,
+        214,
+        150,
+        50,
+        194,
+    ];
+
+    private readonly IDeterminedHash _idHash;
+
+    private readonly IDeterminedHash _chartIdHash;
+
+    private readonly IDeterminedHash _legendHash;
+
+    public AxisRelationalModelHash(IAxisRelationalModel model)
+        : this(model.Id, model.ChartId, model.Legend) { }
+
+    public AxisRelationalModelHash(IGuid id, IGuid chartId, IString legend)
+        : this(
+            new DeterminedHash(id),
+            new DeterminedHash(chartId),
+            new DeterminedHash(legend)
+        )
+    { }
+
+    public AxisRelationalModelHash(
+        IDeterminedHash idHash,
+        IDeterminedHash chartIdHash,
+        IDeterminedHash legendHash
+    )
+    {
+        _idHash = idHash;
+        _chartIdHash = chartIdHash;
+        _legendHash = legendHash;
+    }
+
+    public IEnumerator<byte> GetEnumerator()
+    {
+        return new DeterminedHash(
+            TypePrefix.Concat(_idHash).Concat(_chartIdHash).Concat(_legendHash)
+        ).GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
